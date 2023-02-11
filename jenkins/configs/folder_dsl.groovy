d8fa71d3b555d7a15d3035d553d44477730b5b07
Léo Brossard <leo_brossard@hotmail.fr>
2023-02-11 16:02:29 +0100

    Jenkins base instance

---
+++ jenkins/configs/folder_dsl.groovy
@@ -0,0 +1,7 @@
+folder("Whanos base images") {
+	description("Folder containing all the whanos base images.")
+}
+
+folder("Projects") {
+	description("Folder containing all the projects.")
+}
---
+++ jenkins/configs/job_dsl.groovy
@@ -0,0 +1,49 @@
+languages = ["c", "java", "javascript", "python", "befunge"]
+
+languages.each { language ->
+	freeStyleJob("Whanos base images/whanos-$language") {
+		steps {
+			shell("docker build /images/$language -f /images/$language/Dockerfile.base -t whanos-$language")
+		}
+	}
+}
+
+freeStyleJob("Whanos base images/Build all base images") {
+	publishers {
+		downstream(languages.collect { language -> "Whanos base images/whanos-$language" })
+	}
+}
+
+freeStyleJob("link-project") {
+	parameters {
+		stringParam("GIT_URL", null, 'Git repository url (e.g.: "https://github.com/skrilax91/Kappa-Engine")')
+        stringParam("GIT_BRANCH", "main", 'Git branch (e.g.: "main") (default: "main")')
+		stringParam("NAME", null, "Name for the job")
+	}
+	steps {
+		dsl {
+			text('''
+				freeStyleJob("Projects/$NAME") {
+					scm {
+						git {
+                            branch("$GIT_BRANCH")
+							remote {
+								name("origin")
+								url("$GIT_URL")
+							}
+						}
+					}
+					triggers {
+						scm("* * * * *")
+					}
+					wrappers {
+						preBuildCleanup()
+					}
+					steps {
+						shell("/jenkins/deploy.sh \\"$NAME\\"")
+					}
+				}
+			''')
+		}
+	}
+}
