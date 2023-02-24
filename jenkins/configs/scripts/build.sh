2d4709d6c0a51027d726924d6b4d475f80cd9700
skrilax91 <leo_brossard@hotmail.fr>
2023-02-24 23:42:59 +0100

    fix(image): i suck

--- jenkins/configs/scripts/build.sh
+++ jenkins/configs/scripts/build.sh
@@ -41,7 +41,7 @@ echo ${LANGUAGE[@]} matched
 ## Build the image
 
 # Get the image name
-image_name=whanos-$1
+image_name=whanos-${LANGUAGE[@]}
 
 if [[ -f Dockerfile ]]; then
     echo "Using Dockerfile"
